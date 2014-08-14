c390d3abcb16758fca9dd4d3b16c929bcc3fed3e
Pasang Sherpa <pasang_sh@hotmail.com>
2014-08-14 01:26:43 -0700

    editorconfig fix for json

--- src/linked-list-adt.js
+++ src/linked-list-adt.js
@@ -1,93 +1,93 @@
 (function() {
-    'use strict';
-    var LinkedList = function() {
-        var root = null;
-        var count = 0;
+	'use strict';
+	var LinkedList = function() {
+		var root = null;
+		var count = 0;
 
-        function Node(element) {
-            this.element = element || null;
-            this.next = null;
-        }
+		function Node(element) {
+			this.element = element || null;
+			this.next = null;
+		}
 
-        function add(element) {
-            var node = new Node(element),
-                current = null;
-            if (root === null) {
-                root = node;
-            } else {
-                current = root;
-                while (current.next !== null) {
-                    current = current.next;
-                }
-                current.next = node;
-            }
-            count++;
-        }
+		function add(element) {
+			var node = new Node(element),
+				current = null;
+			if (root === null) {
+				root = node;
+			} else {
+				current = root;
+				while (current.next !== null) {
+					current = current.next;
+				}
+				current.next = node;
+			}
+			count++;
+		}
 
-        function remove(index) {
-        	if (typeof index !== 'undefined') {
-	        	root = removeAt(index, root);
-        	} else {
-        		root = removeAt(count - 1, root);
-        	}
-            count--;
-        }
+		function remove(index) {
+			if (typeof index !== 'undefined') {
+				root = removeAt(index, root);
+			} else {
+				root = removeAt(count - 1, root);
+			}
+			count--;
+		}
 
-        function removeAt(index, node) {
-        	if (node === null) {
-        		throw new Error('removeAt(): No such element found.');
-        	} else if (index === 0) {
-        		return node.next;
-        	} else {
-        		node.next = removeAt(index - 1, node.next);
-        	}
-        	return node;
-        }
+		function removeAt(index, node) {
+			if (node === null) {
+				throw new Error('removeAt(): No such element found.');
+			} else if (index === 0) {
+				return node.next;
+			} else {
+				node.next = removeAt(index - 1, node.next);
+			}
+			return node;
+		}
 
-        function first() {
-            if (isEmpty()) {
-                throw new Error('first(): List is empty.');
-            }
-            return root;
-        }
+		function first() {
+			if (isEmpty()) {
+				throw new Error('first(): List is empty.');
+			}
+			return root;
+		}
 
-        function get(index) {
-            if (index < 0 || index > count || root === null) {
-                return null;
-            }
-            var current = root,
-                i = 0;
-            for (; i < index; i++) {
-                current = current.next;
-            }
-            return current;
-        }
+		function get(index) {
+			if (index < 0 || index > count || root === null) {
+				return null;
+			}
+			var current = root,
+				i = 0;
+			for (; i < index; i++) {
+				current = current.next;
+			}
+			return current;
+		}
 
-        function isEmpty() {
-            return count === 0;
-        }
+		function isEmpty() {
+			return count === 0;
+		}
 
-        function size() {
-            return count;
-        }
+		function size() {
+			return count;
+		}
 
-        return {
-            add: add,
-            remove: remove,
-            first: first,
-            get: get,
-            isEmpty: isEmpty,
-            size: size
-        };
-    };
+		return {
+			add: add,
+			remove: remove,
+			first: first,
+			get: get,
+			isEmpty: isEmpty,
+			size: size
+		};
+	};
 
-    if (typeof define === 'function' && define.amd) {
-        define(function() {
-            return LinkedList;
-        });
-    } else if (typeof module !== 'undefined' && module.exports) {
-        module.exports = LinkedList;
-    } else {
-        window.LinkedList = LinkedList;
-    }
+	if (typeof define === 'function' && define.amd) {
+		define(function() {
+			return LinkedList;
+		});
+	} else if (typeof module !== 'undefined' && module.exports) {
+		module.exports = LinkedList;
+	} else {
+		window.LinkedList = LinkedList;
+	}
 })();
